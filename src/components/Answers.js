d6493e0ae7d7ed65c773db525645975f3df9c9ab
Ralph <ralph@rgalan.es>
2018-07-13 17:06:56 +0100

    Refactor to functional Answers component

--- src/components/Answers.js
+++ src/components/Answers.js
@@ -1,35 +1,22 @@
 import React from 'react';
 
-class Answers extends React.Component {
-    constructor(props) {
-        super(props);
-        this.createQuestionMarkup = this.createQuestionMarkup.bind(this);
-    }
+function Answers({ answers }) {
 
-    // componentDidMount() {
-    //     this.props.requestQuestion();
-    // }
-
-    createQuestionMarkup(question) {
+    function createQuestionMarkup(question) {
         return { __html: question };
     };
 
-    render() {
-        const { answers } = this.props;
-        const question_answers = answers
-            ? [...answers.results[0].incorrect_answers, answers.results[0].correct_answer]
-            : [];
-
-        const shuffled_questions = question_answers.sort(() => 0.5 - Math.random());
-
-        return (
-            <div>
-                {shuffled_questions.map(answer => {
-                    return <button key={answer} dangerouslySetInnerHTML={this.createQuestionMarkup(answer)} />
-                })}
-            </div>
-        )
-    }
+    const question_answers = answers
+        ? [...answers.results[0].incorrect_answers, answers.results[0].correct_answer]
+        : [];
+    const shuffled_questions = question_answers.sort(() => 0.5 - Math.random());
+    return (
+        <div>
+            {shuffled_questions.map(answer => {
+                return <button key={answer} dangerouslySetInnerHTML={createQuestionMarkup(answer)} />
+            })}
+        </div>
+    )
 }
 
 export default Answers;
