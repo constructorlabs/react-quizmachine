c6a6ac3039691064e2ca2c728ad9ab2bd7c97301
Ralph <ralph@rgalan.es>
2018-07-16 09:49:23 +0100

    Added navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Navigation from './Navigation';
+
 import '../static/styles/header.scss';
 
 function Header() {
     return (
         <div className="main-header">
+            <Navigation />
             <h1 className="main-header__logo">Quiz</h1>
             <div className="main-header__subtext">Machine</div>
         </div>
---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import "../static/styles/navigation.scss";
+
+function Navigation(props) {
+    return (
+        <div className="navigation">
+            <Link to="/quiz" className="navigation__item">Quiz</Link>
+            <Link to="/hall-of-fame" className="navigation__item">Hall Of Fame</Link>
+        </div>
+    )
+}
+export default Navigation;
