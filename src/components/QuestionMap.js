25bf78ef4bf48ce4ef158b9e9088e69958afd86f
Luke Speirs <Luke@Lukes-MacBook-Pro.local>
2018-11-02 15:29:26 +0000

    FIXEDgit add .git add .git add .

--- src/components/QuestionMap.js
+++ src/components/QuestionMap.js
@@ -15,83 +15,23 @@ class QuestionMap extends React.Component {
   }
 
   render() {
-    return ( 
-    <p className="score">Score: {this.props.score}</p> 
-    
-    {this.props.question.map(question => {
-      if (question.type == "boolean") {
-        return (
-          <div className="question" key={question.question}>
-            <p>{question.category}</p>
-            <p>{question.difficulty.toUpperCase()}</p>
-            <p>{question.question}</p>
+    console.log("props", this.props);
+    return (
+      <div className="grid">
+        <p>Score: {this.props.score}</p>
+        {this.props.question.map(question => {
+          if (question.type == "boolean") {
+            return (
+              <div key={question.question}>
+                <p>{question.category}</p>
+                <p>{question.difficulty.toUpperCase()}</p>
+                <p>{question.question}</p>
 
-            <div className="true-false-grid">
-              <button
-                className="voting-button button-one"
-                type="button"
-                value="True"
-                onClick={event => {
-                  event.preventDefault();
-                  this.props.answerClickHandle(
-                    event.target.value,
-                    this.state.difficulty,
-                    this.state.category
-                  );
-                }}
-              >
-                True
-              </button>
-              <button
-                className="voting-button button-two"
-                type="button"
-                value="False"
-                onClick={event => {
-                  event.preventDefault();
-                  this.props.answerClickHandle(
-                    event.target.value,
-                    this.state.difficulty,
-                    this.state.category
-                  );
-                }}
-              >
-                False
-              </button>
-            </div>
-          </div>
-        );
-      } else {
-        return (
-          <div className="question" key={question.question}>
-            <p>{question.category}</p>
-            <p>{question.difficulty.toUpperCase()}</p>
-            <p>{question.question}</p>
-
-            <div>
-              <button
-                className="voting-button"
-                type="button"
-                key={question.correct_answer}
-                value={question.correct_answer}
-                onClick={event => {
-                  event.preventDefault();
-                  this.props.answerClickHandle(
-                    event.target.value,
-                    this.state.difficulty,
-                    this.state.category
-                  );
-                }}
-              >
-                {question.correct_answer}
-              </button>
-
-              {question.incorrect_answers.map(answer => {
-                return (
+                <div className="true-false-grid">
                   <button
-                    className="voting-button"
+                    className="voting-button button-one"
                     type="button"
-                    key={answer}
-                    value={answer}
+                    value="True"
                     onClick={event => {
                       event.preventDefault();
                       this.props.answerClickHandle(
@@ -101,16 +41,76 @@ class QuestionMap extends React.Component {
                       );
                     }}
                   >
-                    {answer}
+                    True
                   </button>
-                );
-              })}
-            </div>
-          </div>
-        );
-    }
-    })}
-    })
-    }
+                  <button
+                    className="voting-button button-two"
+                    type="button"
+                    value="False"
+                    onClick={event => {
+                      this.props.answerClickHandle(
+                        event.target.value,
+                        this.state.difficulty,
+                        this.state.category
+                      );
+                    }}
+                  >
+                    False
+                  </button>
+                </div>
+              </div>
+            );
+          } else {
+            return (
+              <div key={question.question}>
+                <p>{question.category}</p>
+                <p>{question.difficulty.toUpperCase()}</p>
+                <p>{question.question}</p>
+
+                <div>
+                  <button
+                    className="voting-button"
+                    type="button"
+                    key={question.correct_answer}
+                    value={question.correct_answer}
+                    onClick={event => {
+                      this.props.answerClickHandle(
+                        event.target.value,
+                        this.state.difficulty,
+                        this.state.category
+                      );
+                    }}
+                  >
+                    {question.correct_answer}
+                  </button>
+
+                  {question.incorrect_answers.map(answer => {
+                    return (
+                      <button
+                        className="voting-button"
+                        type="button"
+                        key={answer}
+                        value={answer}
+                        onClick={event => {
+                          this.props.answerClickHandle(
+                            event.target.value,
+                            this.state.difficulty,
+                            this.state.category
+                          );
+                        }}
+                      >
+                        {answer}
+                      </button>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          }
+        })}
+      </div>
+    );
+  }
+}
 
 export default QuestionMap;
