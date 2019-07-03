00a4d1efdb7c87c2ebd4e1c372dee946e9a29ee0
sheila <sheilahk@hotmail.com>
2019-07-03 08:38:28 +0100

    Suggested fixes

--- src/containers/QuestionContainer.js
+++ src/containers/QuestionContainer.js
@@ -1,9 +1,18 @@
 import { connect } from "react-redux";
 import Question from "../components/Question";
-import { fetchQuestionFromAPI, checkAnswer, endQuestions, endGame, friendline, audienceline, fiftyQuestions, restart } from "../actions";
+import {
+  fetchQuestionFromAPI,
+  checkAnswer,
+  endQuestions,
+  endGame,
+  friendline,
+  audienceline,
+  fiftyQuestions,
+  restart
+} from "../actions";
 
-const mapStateToProps = (reduxState) => {
-  console.log("container reply", reduxState.reply)
+const mapStateToProps = reduxState => {
+  // console.log("container reply", reduxState.reply)
   return {
     quizData: reduxState.question,
     money: reduxState.money,
@@ -16,32 +25,39 @@ const mapStateToProps = (reduxState) => {
 };
 
 const mapDispatchToProps = dispatch => {
-
   return {
     fetchQuestion: () => dispatch(fetchQuestionFromAPI()),
-    correctAnswerFn: (status) => {
-      dispatch(checkAnswer('CORRECT_ANSWER')),
+    correctAnswerFn: status => {
+      dispatch(checkAnswer("CORRECT_ANSWER")),
         dispatch(fetchQuestionFromAPI()),
-        dispatch(endGame(status))
+        dispatch(endGame(status));
     },
-    incorrectAnswerFn: (status) => {
-      dispatch(checkAnswer('INCORRECT_ANSWER')),
+    incorrectAnswerFn: status => {
+      dispatch(checkAnswer("INCORRECT_ANSWER")),
         dispatch(endQuestions()),
-        dispatch(endGame(status))
+        dispatch(endGame(status));
     },
     walkFn: () => {
-      dispatch(checkAnswer('WALK')),
+      dispatch(checkAnswer("WALK")),
         dispatch(endQuestions()),
-        dispatch(endGame('LOSE'))
+        dispatch(endGame("LOSE"));
     },
-    friendLine: (help) => { dispatch(friendline(help)) },
-    audienceLine: (help) => { dispatch(audienceline(help)) },
-    fiftyLine: (help) => { dispatch(fiftyQuestions(help)) },
+    friendLine: help => {
+      dispatch(friendline(help));
+    },
+    audienceLine: help => {
+      dispatch(audienceline(help));
+    },
+    fiftyLine: help => {
+      dispatch(fiftyQuestions(help));
+    },
     newGame: () => {
-      dispatch(restart()),
-        dispatch(fetchQuestionFromAPI())
+      dispatch(restart()), dispatch(fetchQuestionFromAPI());
     }
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(Question);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Question);
